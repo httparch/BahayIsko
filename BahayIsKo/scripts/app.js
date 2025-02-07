d7c71bc597a68debe3c1a6377b60174b3d94c1ca
httparch <shanebigornia123@gmail.com>
2025-02-07 10:54:17 +0800

    done w/ fxnality, need to optimize

--- BahayIsKo/scripts/app.js
+++ BahayIsKo/scripts/app.js
@@ -12,8 +12,10 @@ let index = 0;
 
 function nextQuestion() {
     if (index >= quiz.length) {
-        calculateResult()
-        quiz_page.innerHTML = `<p>Congratulations! You've completed the quiz.</p>`;
+        
+        console.log(calculateResult())
+        let result = describeResult(calculateResult())
+        quiz_page.innerHTML = `<p>Congratulations! You've completed the quiz.</p> ${result}`;
         return;
     }
 
@@ -35,9 +37,64 @@ function nextQuestion() {
 }
 
 function calculateResult(){
-    myArray.forEach(item => {
 
+    let houseCount = [0, 0, 0, 0, 0];
+    let arrayOfHouse = [];
+    let otherHouse;
+
+    myArray.forEach(item => {
+        houseCount[item - 1]++;
     })
+
+    let maxCount = Math.max(...houseCount);
+    let maxCountIndex = houseCount.indexOf(Math.max(...houseCount));
+    arrayOfHouse.push(maxCountIndex+1)
+    
+    for(var i = 0; i < houseCount.length -1; i++){
+        if((maxCount == houseCount[i]) && maxCountIndex != i){
+            otherHouse = i+1;
+            arrayOfHouse.push(otherHouse);
+        }
+    }
+
+    return arrayOfHouse;
+}
+
+function getMeaning(index){
+    let meaning = ``;
+    switch(index){
+        case 1:
+            meaning = `Hawks`;
+            break;
+        case 2:
+            meaning = `Wolves`;
+            break;
+        case 3:
+            meaning = `Foxes`;
+            break;
+        case 4:
+            meaning = `Dolphins`;
+            break;
+        case 5:
+            meaning = `Owls`;
+            break;
+        default:
+            break;
+    }
+
+    return meaning;
+}
+function describeResult(arrayOfHouse){
+
+    let result = ``;
+
+    if(arrayOfHouse.length == 1){ //solo
+        result = `You are a ${getMeaning(arrayOfHouse[0])}`;
+    }else if(arrayOfHouse.length == 2){ //dual
+        result = `You are a both ${getMeaning(arrayOfHouse[0])} and ${getMeaning(arrayOfHouse[1])}`;
+    }
+
+    return result;
 }
 
 
